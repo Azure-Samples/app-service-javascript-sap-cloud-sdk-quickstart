a369e94d35fbdd5b9987bd37e4d79c0f97e517ac
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-02-18 17:17:46 +0100

    add predeployhook config

--- src/api/src/app.controller.spec.ts
+++ src/api/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe("AppController", () => {
 	});
 
 	let result: object = { title: "SAP Cloud SDK on Azure", message: "Hello Azure! Greetings from the" };
-
+	
 	describe("root", () => {
 		it('should return result object', async () => {
 			expect(await appController.getHello()).toMatchObject(result);
