2515f803e66fdc2325f1feaf276ca86cbc9b7819
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-02-28 17:44:31 +0100

    cleanup of test configs

--- src/api/src/business-partner/business-partner.controller.spec.ts
+++ src/api/src/business-partner/business-partner.controller.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { BusinessPartnerController } from "./business-partner.controller";
+import { BusinessPartnerService } from "./business-partner.service";
 
 describe("BusinessPartnerController", () => {
 	let controller: BusinessPartnerController;
+	let service: BusinessPartnerService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [BusinessPartnerController],
+			providers: [BusinessPartnerService],
 		}).compile();
 
 		controller = module.get<BusinessPartnerController>(
-			BusinessPartnerController,
+			BusinessPartnerController
 		);
 	});
 
